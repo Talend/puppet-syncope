d973b78776b98c2773aae22cb7a5f92dc7b8f3ac
aheumaier <andreas.heumaier@nordcloud.com>
2016-05-27 20:08:42 +0200

    FIX for master branch

--- manifests/install.pp
+++ manifests/install.pp
@@ -7,13 +7,6 @@ class syncope::install () {
       ensure => installed;
     'syncope-sts':
       ensure => installed;
-  } ->
-
-  exec{'move_syncope_to_the_right_catalina_home':
-    command  => 'cp -R ',
-    creates  => '/var/lock/postgres_jar_link.lock',
-    provider => 'shell',
-    notify   => Service['tomcat-syncope-srv'],
   }
-  
+
 }
