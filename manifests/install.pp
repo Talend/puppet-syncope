b56fd084698592c0b09c3ca86af7a1682d611a13
aheumaier <andreas.heumaier@nordcloud.com>
2016-05-31 10:22:26 +0200

    WIP introduce require in packages again

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,10 +2,13 @@ class syncope::install () {
 
   package {
     'syncope':
+      require => Tomcat::Instance['tomcat'],
       ensure => installed;
     'syncope-console':
+      require => Tomcat::Instance['tomcat'],
       ensure => installed;
     'syncope-sts':
+      require => Tomcat::Instance['tomcat'],
       ensure => installed;
   }
 
