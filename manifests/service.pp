0851dc5332cc266c839183a58d486bdfcb72d06c
Alexander Grigoriev <agrigoriev@talend.com>
2016-12-02 13:06:32 +0100

    adding option to set service state

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,11 +1,14 @@
 class syncope::service (
 
-  $catalina_base = $syncope::catalina_base
+  $catalina_base  = $syncope::catalina_base,
+  $service_ensure = running,
 
 ) {
 
   tomcat::service { 'syncope':
-    catalina_base => $catalina_base,
-    use_init      => false,
+    service_ensure => $service_ensure,
+    catalina_base  => $catalina_base,
+    use_init       => false,
   }
+
 }
